3b41bccfdf1608c504ff0e228b3e96bd06e0c68b
sklccchenchen <2934401021@qq.com>
2019-02-10 22:38:26 +0800

    modified

--- app/index.js
+++ app/index.js
@@ -1,9 +1,10 @@
 import _ from 'lodash'
+import $ from 'jquery'
 
 function component(){
-    var element = document.createElement('div')
-    element.innerHTML = _.join(['hello','webpack'],' ')
-    return element
+    var element = $('<div></div>')
+    element.html(_.join(['hello','webpack'],' '))
+    return element.get(0)
 }
 
 document.body.appendChild(component())
