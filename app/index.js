3e37db5047e880c1952e2cb3dd96f51b8aa77792
sklccchenchen <2934401021@qq.com>
2019-02-10 22:52:55 +0800

    modified

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash'
 import $ from 'jquery'
+import foo from './foo'
 
 function component(){
     var element = $('<div></div>')
@@ -8,3 +9,5 @@ function component(){
 }
 
 document.body.appendChild(component())
+console.log(foo)
+console.log(foo())
